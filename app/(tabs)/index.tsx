e7118dadfdf0c87483e478d58318e0df2f72abf7
AnaclaudiaSac <anaclaudiasacute75@gmail.com>
2024-11-27 18:28:10 -0300

    feat: include if

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
 import { View, Text, StyleSheet, Button } from "react-native";
 import { router } from "expo-router";
-("expo-router");
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
-      <Text onPress={() => router.back()}>Voltar para o Index</Text>
+      <Text
+        onPress={() => {
+          const canGoBack = router.canGoBack();
+          console.log(canGoBack);
+          if (canGoBack) {
+            router.back();
+          }
+        }}
+      >
+        Voltar para o Index
+      </Text>
     </View>
   );
 }
